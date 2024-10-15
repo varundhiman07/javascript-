da4c263eac1e23656be2c1eea6a9ec03c5b78b72
varundhiman07 <dhimanvarun71@gmail.com>
2024-10-15 07:03:03 +0000

    conversion

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,18 @@
+let score = 33;
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let hello = "33abc"
+
+console.log (typeof (hello));
+
+let valueInNumber = Number(hello);
+console.log(typeof(valueInNumber));
+console.log(valueInNumber);    //shows nan not a number //
+
+//33=> number 
+//33abc => not a number but shows datatype number
+//true=> 1 and false=> 0 
+//undefined => not a number
+//null => 0
