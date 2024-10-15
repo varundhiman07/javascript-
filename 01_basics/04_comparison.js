30ae098615a4a44b134260e8539d11f58f0422ba
varundhiman07 <dhimanvarun71@gmail.com>
2024-10-15 09:48:37 +0000

    dataypes

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,12 @@
+console.log("2" < 1);
+console.log("02" > 2);
+console.log("02" < 2);
+console.log(typeof  ("02" > 4));
+
+console.log(null > 0 );
+console.log(null < 0 );
+console.log(null >= 0);
+
+console.log("3" === 3);
+console.log("mani" == 4);
+console.log(typeof ("mani"=== "4"));
---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,10 @@
+/*  datatypes type =>
+two types :
+primitive and non primitive
+
+primitive (call by value ): strings , number , boolean , null , undefined , symbol , BigInt
+non primitive (call by reference ): array , objects , functions 
+    */
+
+let value = null;
+console.log(typeof (value));
