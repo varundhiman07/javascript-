798b90e2778a85060e399989c9df59e873fc536d
varundhiman07 <dhimanvarun71@gmail.com>
2024-10-15 07:44:59 +0000

    conversion and operation

--- 01_basics/03_conversionoperation.js
+++ 01_basics/03_conversionoperation.js
@@ -3,11 +3,11 @@ let score = 33;
 console.log(typeof score);
 console.log(typeof (score));
 
-let hello = "33abc"
+let value = "null"
 
-console.log (typeof (hello));
+console.log (typeof (value));
 
-let valueInNumber = Number(hello);
+let valueInNumber = Number(value);
 console.log(typeof(valueInNumber));
 console.log(valueInNumber);    //shows nan not a number //
 
@@ -16,3 +16,25 @@ console.log(valueInNumber);    //shows nan not a number //
 //true=> 1 and false=> 0 
 //undefined => not a number
 //null => 0
+
+//                           operations                            //
+
+let str1 = 2;
+let str2 = 4;
+ 
+let str3 = str1 + str2;
+
+console.log(str3);
+ 
+console.log("1" + 2); // add according to string 
+console.log(1 + "2"); // add according to string
+console.log("1" + 2 + 2); //add according to string 
+console.log(1 + 2 + "2"); // first add the number then add the string *important 
+console.log(3 * 2 + "4"); // first multiply the number then add according to string
+
+let number = "mani";
+let  alphabet = 4;
+let plus = number + alphabet;
+console.log(typeof (plus));
+console.log(plus);
+
